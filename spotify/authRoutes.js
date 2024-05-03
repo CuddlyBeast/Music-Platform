818fb370bbaab4e809124dfb7614e784ab957d7e
CuddlyBeast <147174642+CuddlyBeast@users.noreply.github.com>
2024-05-03 17:16:00 +0100

    trying to make the redirect work

--- spotify/authRoutes.js
+++ spotify/authRoutes.js
@@ -6,15 +6,15 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const spotifyApi = new SpotifyWebApi({
     clientId: '1e69079ae7904703a2c03ecab6c95f99',
     clientSecret: '884af1572aaf41b7a20721b833844e63',
-    redirectUri: 'http://youcountry.herokuapp.com/chill/callback'
+    redirectUri: 'http://youcountry-f1df846a7cc3.herokuapp.com/chill/callback'
   });
 
 // local env redirect:  'http://localhost:3000/chill/callback'
 
 router.get('/auth', (req, res) => {
     const clientId = '1e69079ae7904703a2c03ecab6c95f99';
-    const redirectUri = 'http://youcountry.herokuapp.com/chill/callback'; 
-    const scopes = ['user-read-private', 'user-read-email', 'user-read-recently-played', 'user-top-read', 'user-modify-playback-state', 'playlist-read-private', 'user-library-read', 'user-library-modify', 'user-follow-read', 'playlist-modify-public', 'playlist-modify-private', 'streaming', 'user-read-playback-state' ]; // Specify the required scopes
+    const redirectUri = 'http://youcountry-f1df846a7cc3.herokuapp.com/chill/callback'; 
+    const scopes = ['user-read-private', 'user-read-email', 'user-read-recently-played', 'user-top-read', 'user-modify-playback-state', 'playlist-read-private', 'user-library-read', 'user-library-modify', 'user-follow-read', 'playlist-modify-public', 'playlist-modify-private', 'streaming', 'user-read-playback-state' ]; 
     const spotifyAuthUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}`;
 
     res.redirect(spotifyAuthUrl);
@@ -40,7 +40,7 @@ router.get('/callback', async (req, res) => {
 
         console.log('Access token and refresh token set.');
 
-        res.redirect(`http://youcountry.herokuapp.com/?access_token=${access_token}&refresh_token=${refresh_token}`)
+        res.redirect(`http://youcountry-f1df846a7cc3.herokuapp.com/?access_token=${access_token}&refresh_token=${refresh_token}`)
     } catch (error) {
         console.error('Error:', error);
         res.status(500).send('Authentication failed. Please try again.');
