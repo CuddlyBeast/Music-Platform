abeab56219e2c8707a69f0a8107f00a3974aee91
CuddlyBeast <147174642+CuddlyBeast@users.noreply.github.com>
2024-05-06 17:35:50 +0100

    index sequelize config change

--- models/index.js
+++ models/index.js
@@ -3,9 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const process = require('process');
 const basename = path.basename(__filename);
-// const env = process.env.NODE_ENV || 'development';
 const env = process.env.NODE_ENV || 'production';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
@@ -14,16 +12,12 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], {
     ...config,
-    dialectOptions: {
-      ssl: true
-    }
+    dialectOptions: config.dialectOptions || { ssl: true }
   });
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, {
     ...config,
-    dialectOptions: {
-      ssl: true
-    }
+    dialectOptions: config.dialectOptions || { ssl: true }
   });
 }
 
