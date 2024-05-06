9367e05e0974f62bd74f579d6059b9a49f68336a
CuddlyBeast <147174642+CuddlyBeast@users.noreply.github.com>
2024-05-06 18:23:08 +0100

    test on user table

--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.hasMany(models.Playlist, { foreignKey: 'userId', as: 'playlists' });
+      this.hasMany(models.Playlist, { foreignKey: 'userId' });
     }
   }
   User.init({
